0879b237ac822dbadf1c6841e9c491bedb82657b
Daniel Nyberg <daniel.nyberg86@gmail.com>
2025-09-11 12:39:26 +0200

    Fix location and add error handler for blogpost

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const blogpageRoutes = require('./routes/blogpage');
+app.use('/', blogpageRoutes);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
