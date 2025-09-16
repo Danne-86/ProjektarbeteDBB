3bd890d4d4430679d23c3078df2b58f9ded6b298
Daniel Nyberg <daniel.nyberg86@gmail.com>
2025-09-16 10:17:23 +0200

    Fixed route

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var path = require('path');
 // Configure Multer for image uploads
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/images/');
+    cb(null, 'public/img/');
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
