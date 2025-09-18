3dab8dd1e0d94d863e1e27d76f20bb8495d6b812
Nirodie <hanneswow98@hotmail.com>
2025-09-18 14:20:14 +0200

    Now takes avatar from database

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const db = require('better-sqlite3')('database.db');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const posts = db.prepare('SELECT * FROM posts').all();
+  const posts = db.prepare(`
+        SELECT p.*, u.avatar 
+        FROM posts p
+        INNER JOIN users u ON p.user_id = u.id
+    `).all();
 
   res.render('index', {
     title: 'Inkflow',
