42b55c3740f6134e18b141611f0d80b668eb5a4e
Daniel Nyberg <daniel.nyberg86@gmail.com>
2025-09-23 09:24:40 +0200

    Fix routes

--- app.js
+++ app.js
@@ -96,12 +96,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routers
 
-app.use("/blog", blogRouter);
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/", authRouter);
 app.use("/admin", adminRouter);
 app.use("/profile", profileRouter);
+app.use("/blog", blogRouter);
 
 // 404
 app.use(function (req, res, next) {
