e79403dc8974426a76bce58b2e2e27249b41d9a3
Daniel Nyberg <daniel.nyberg86@gmail.com>
2025-09-23 13:02:25 +0200

    remove route

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,6 @@ const { uploadPostImage } = require("../middleware/upload");
 const { error } = require("console");
 const { authenticateToken } = require("../middleware/auth");
 
-//  Root redirects to /feed
-router.get("/", (req, res) => res.redirect("/feed"));
-
 router.get("/", (req, res) => {
   res.render("", {
     title: "Landing",
